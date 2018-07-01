68363a09e4fa4d6abe0b3ee82cd11a030c00cf3a
missxie <missxie523@gmail.com>
2018-07-01 17:28:13 -0400

    tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Tabs, Button, Spin } from 'antd';
+
+const TabPane = Tabs.TabPane;
+const operations = <Button type="primary">Create New Post</Button>;
 
 export class Home extends React.Component {
     render() {
         return (
-            <div>this is home</div>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Posts" key="1"></TabPane>
+                <TabPane tab="Map" key="2">Content of tab 2</TabPane>
+            </Tabs>
         );
     }
+
 }
